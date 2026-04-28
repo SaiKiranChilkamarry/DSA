863049d4b7e745735b27d74d81b51517f7a7f973
SAI KIRAN CHILKAMARRY <kiran333408@gmailm.com>
2026-04-28 22:47:50 +0530

    only element with one time appearence and all other appear twice in this situation we can find that number using hashmap and optimal way is to use XOR operation to find the element

---
+++ arrays/freq_one.py
@@ -0,0 +1,30 @@
+"""Example 1:
+Input Format: arr[] = {2,2,1}
+Result: 1
+Explanation: In this array, only the element 1 appear once and so it is the answer.
+
+
+Example 2:
+Input Format: arr[] = {4,1,2,1,2}
+Result: 4
+Explanation: In this array, only element 4 appear once and the other elements appear twice. So, 4 is the answer.
+"""
+
+def frequency(array):
+  freq={}
+  for num in array:
+    freq[num]=freq.get(num,0)+1
+
+  for key,value in freq.items():
+    if value == 1:
+      return key 
+
+
+
+array=[4,1,2,1,2]
+result = frequency(array)
+print("the element that appears only once in an array is ",result)
+"""
+time complexity o(n)
+space complexity o(n)
+"""
