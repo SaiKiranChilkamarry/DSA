e739b45a73485f531959fc1ff1bb356855f39aa4
SAI KIRAN CHILKAMARRY <kiran333408@gmailm.com>
2026-04-28 17:47:28 +0530

    left ratation of array by k elements

---
+++ arrays/left_rotation_array.py
@@ -0,0 +1,28 @@
+"""Input : nums = [1, 2, 3, 4, 5, 6, 7], k = 2, right
+Output : [6, 7, 1, 2, 3, 4, 5]
+Explanation : rotate 1 step to the right: [7, 1, 2, 3, 4, 5, 6]
+rotate 2 steps to the right: [6, 7, 1, 2, 3, 4, 5] 
+
+Input : nums = [1, 2, 3, 4, 5, 6], k=2, left
+Output : [3, 4, 5, 6, 1, 2]
+Explanation :rotate 1 step to the left: [2, 3, 4, 5, 6, 1]
+rotate 2 steps to the left: [3, 4, 5, 6, 1, 2]
+"""
+def left_rotation(array,n,k):
+  k=k%n
+  if k==0:
+    return 
+  temp=[]
+  for i in range (k):
+    temp.append(array[i])
+  for i in range(k,n):
+    array[i-k]=array[i]
+  for i in range(len(temp)):
+    array[n-k+i]=temp[i]
+  
+
+
+array = [1, 2, 3, 4, 5, 6, 7]
+k=43
+left_rotation(array,len(array),k)
+print(array)
