4f3763bc2b8d6336d2171a97c92b18768f2a1c34
SAI KIRAN CHILKAMARRY <kiran333408@gmailm.com>
2026-04-28 16:57:07 +0530

    the optimal way to find largest elemnt in an array

---
+++ arrays/largest_number.py
@@ -0,0 +1,36 @@
+"""Example 1:
+Input:
+ arr[] = {2, 5, 1, 3, 0}  
+Output:
+ 5  
+Explanation:
+  
+5 is the largest element in the array.
+
+Example 2:
+Input:
+ arr[] = {8, 10, 5, 7, 9}  
+Output:
+ 10  
+Explanation:
+  
+10 is the largest element in the array.
+
+Brute force approch => sort array return last element 
+
+program for better approch
+"""
+def find_largest(array):
+  if not len(array):
+    print ("array is empty")
+    return None 
+  largest=array[0]
+  for i in range(1,len(array)):
+    if array[i]>largest:
+      largest=array[i]
+  return largest 
+
+
+array= [2, 5, 1, 3, 0]
+Largest=find_largest(array)
+print("the largest number in the array found is  ",Largest)
