c4e86fa57d6b57debae519184af3b8a79661e186
SAI KIRAN CHILKAMARRY <kiran333408@gmailm.com>
2026-04-28 16:14:12 +0530

    Two sum BruteForce approch

---
+++ arrays/two_sum_bruteforce.py
@@ -0,0 +1,41 @@
+"""
+Problem: Two Sum
+
+Given an array of integers and a target,
+return indices of two numbers such that
+they add up to the target.
+
+
+Time Complexity: O(n^2)
+Space Complexity: O(1)
+"""
+"""
+Problem: Two Sum
+
+Constraints:
+- 2 <= len(arr) <= 10^4
+- -10^9 <= arr[i] <= 10^9
+- -10^9 <= target <= 10^9
+- Exactly one valid solution exists
+- Cannot use the same element twice
+"""
+"Bruteforce approch "
+
+def TwoSumBruteForce(arr,target):
+  for i in range (len(arr)-1):
+    for j in range (i+1,len(arr)):
+      if arr[i]+arr[j]==target:
+        return [i,j]
+
+
+array = [10, 22, 5, 75, 65, 80, 15, 3, 7]
+target = 85
+result=TwoSumBruteForce(array,target)
+print(result)
+
+
+
+
+
+
+
